8e13482c7faedb44d078ff2769540b1ec879cc0c
ankit K <ankit48365@gmail.com>
2024-01-09 22:31:54 -0500

    dag_airbyte_dev DAG is running airbyte job

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,7 @@ FROM quay.io/astronomer/astro-runtime:10.0.0
 # replace dbt-postgres with another supported adapter if you're using a different warehouse type
 RUN python -m venv dbt_venv && source dbt_venv/bin/activate && \
     pip install --no-cache-dir dbt-postgres && deactivate
+
+
+# FROM apache/airflow:2.8.0
+ENV AIRFLOW__CORE__TEST_CONNECTION=Enabled
--- dags/dag_airbyte_dev.py
+++ dags/dag_airbyte_dev.py
@@ -2,20 +2,42 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
-from pendulum import datetime
 
-with DAG(dag_id='trigger_airbyte_job_example',
-         default_args={'owner': 'airflow'},
-         schedule_interval='@daily',
-        #  start_date=days_ago(1)
-         start_date=datetime(2024, 1, 1),
-    ) as dag:
+with DAG(
+    dag_id='Pizza2Postgres_dev',
+    default_args={'owner': 'airflow'},
+    schedule_interval='@daily',
+    start_date=days_ago(1)
+) as dag:
 
-    money_to_json = AirbyteTriggerSyncOperator(
-        task_id='airbyte_money_json_example',
+    airbyte_sync = AirbyteTriggerSyncOperator(
+        task_id='airbyte_sync_example',
         airbyte_conn_id='AirbyteConnection',
-        connection_id='45946a38-bcd1-4237-b5d9-6b47240ee2ab',
-        asynchronous=True,
+        connection_id='440b278b-1b35-495e-9463-e06b5706bd04',
+        asynchronous=False,
         timeout=3600,
         wait_seconds=3
     )
+
+
+
+# from airflow import DAG
+# from airflow.utils.dates import days_ago
+# from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+# from pendulum import datetime
+
+# with DAG(dag_id='trigger_airbyte_job_example',
+#          default_args={'owner': 'airflow'},
+#          schedule_interval='@daily',
+#         #  start_date=days_ago(1)
+#          start_date=datetime(2024, 1, 1),
+#     ) as dag:
+
+#     money_to_json = AirbyteTriggerSyncOperator(
+#         task_id='airbyte_money_json_example',
+#         airbyte_conn_id='AirbyteConnection',
+#         connection_id='45946a38-bcd1-4237-b5d9-6b47240ee2ab',
+#         asynchronous=True,
+#         timeout=3600,
+#         wait_seconds=3
+#     )
